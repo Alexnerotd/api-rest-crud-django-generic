7c683ad8469ea6a64d587d514f2a736c74f8d3b3
Nero <jesalemalo@gmail.com>
2023-09-19 21:41:31 -0400

    I add validation and encrypt for password

--- core/serializers.py
+++ core/serializers.py
@@ -1,2 +1,15 @@
 from rest_framework import serializers
 from .models import *
+
+
+class MyUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = MyUser
+        fields = '__all__'
+
+    def create(self, validate_data):
+        user = MyUser(**validate_data)
+        user.set_password(validate_data['password'])
+        user.save()
+        return user
